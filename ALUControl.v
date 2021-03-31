4730733da84ec0023d8d2be4209e06ba48458d8e
Apoorva <42975271+apurv18@users.noreply.github.com>
2021-03-31 18:27:55 +0530

    Update ALUControl.v

--- ALUControl.v
+++ ALUControl.v
@@ -1,2 +1,5 @@
-module ALUControl(ALUOp, instr5_0, ALUCntrl);
-  
+module ALUControl(ALUOp, Func, ALUCntrl);
+	input [1:0] ALUOp;
+	input [5:0] Func;
+	output reg [3:0] ALUCntrl;		//ALUOp taken 4 bits to include implementation of I type instructions 
+	
