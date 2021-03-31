88be67088ccbdf8821c2b26e20747988b0516288
Apoorva <42975271+apurv18@users.noreply.github.com>
2021-03-31 17:11:15 +0530

    Update ShiftLeft2.v

--- ShiftLeft2.v
+++ ShiftLeft2.v
@@ -1,9 +1,9 @@
 module ShiftLeft2(number_in, number_out);
-  input [31:0] number_in;
-  output [31:0] number_out;
+	input [31:0] number_in;
+	output [31:0] number_out;
   
-  always @(number_in)
-    begin
-		  number_out = number_in << 2;
-	  end 
+	always @(number_in)
+		begin
+			number_out = number_in << 2;		//Shifts input number by 2 bits to the left and assigns it to output
+		end 
 endmodule
