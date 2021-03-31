06f6ec3b8860a71de16e7ef1b75fc314f33421b1
Apoorva <42975271+apurv18@users.noreply.github.com>
2021-03-31 18:40:24 +0530

    Update DataMem.v

--- DataMem.v
+++ DataMem.v
@@ -1,1 +1,20 @@
-module 
+module DataMem(Address, WriteData, MemWrite, MemRead, ReadData, clock);
+  input [31:0] Address;
+  input [31:0] WriteData;
+  input MemWrite;
+  input MemRead;
+  input clock;
+  output reg [31:0] ReadData;
+
+  reg [31:0] RAM [0:255]; //32 bits memory with 255 entries
+	
+	always @ (posedge clock)
+    begin
+		  if (MemWrite == 1)
+        RAM[address[7:2]] <= WriteData;
+  	end
+	always @(negedge clock)
+    begin
+		  if (MemRead == 1)
+        ReadData <= RAM[address[7:2]];
+	end	
