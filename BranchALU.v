6744e700fc3bdca8cdfa688dd4ecbc038608d862
Apoorva <42975271+apurv18@users.noreply.github.com>
2021-03-31 17:20:09 +0530

    Update BranchALU.v

--- BranchALU.v
+++ BranchALU.v
@@ -1,1 +1,8 @@
-
+module Add_ALU(PCOut, number_out, BranchALUOut);
+  input [31:0] PCout;
+  input [31:0] number_out;
+  output reg [31:0] BranchALUOut;
+	always @(*) begin
+		BranchALUOut <= PCout + number_out;     //Adds incremented program counter to the branch amount
+	end
+endmodule
