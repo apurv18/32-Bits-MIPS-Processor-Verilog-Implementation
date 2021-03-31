673776009c6a2e7a60a3d0fbc934780bec33f0c5
Apoorva <42975271+apurv18@users.noreply.github.com>
2021-03-31 17:56:33 +0530

    Update bigALU_MUX.v

--- bigALU_MUX.v
+++ bigALU_MUX.v
@@ -1,1 +1,15 @@
+module bigALU_MUX (ALUSrc, ReadData2, SignExtend32, ALU_B);
 
+	input ALUSrc;
+        input [31:0] ReadData2;
+        input [31:0] SignExtend32;	
+	output reg [31:0] ALU_B;
+	
+        always @(ALUSrc, ReadData2, SignExtend32) 
+                begin
+                        case (ALUSrc)                           //Checks value of Control Line ALUSrc
+			0: ALU_B <= ReadData2;                  //If ALUSrc is set to 0, Data is read from register
+                        1: ALU_B <= Extend32;                   //If ALUSrc is set to 1, Data is taken from sign extended value of Immediate data
+		endcase
+	end
+endmodule
